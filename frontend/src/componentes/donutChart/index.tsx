c01c061043aeb1dd80ede1abc67c5a3ad33e5962
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2021-05-17 17:34:00 -0300

    DonutChart Integration

--- frontend/src/componentes/donutChart/index.tsx
+++ frontend/src/componentes/donutChart/index.tsx
@@ -1,52 +1,53 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts'
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/request';
 
-type ChartData ={
-    labels:string[];
+type ChartData = {
+    labels: string[];
     series: number[];
 }
 
 
-const donutChart = () =>{
-    //forma errada
-    let chartData : ChartData = { labels: [] , series: []};
-    axios.get(`${BASE_URL}/sales/sum-by-seller`)
-         .then( res=>{
-            const data = res.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x=> x.sum)
-            chartData = {
-                labels:myLabels,
-                series: mySeries
-            }
+const DonutChart = () => {
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-            console.log(chartData);
-         });
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/sum-by-seller`)
+            .then(res => {
+                const data = res.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
 
+                setChartData({ labels: myLabels, series: mySeries })
+
+
+            });
+
+    }, []);
 
     //const mockData = {
-      //  series: [477138, 499928, 444867, 220426, 473088],
-        //labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+    //  series: [477138, 499928, 444867, 220426, 473088],
+    //labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     //}
-    
+
     const options = {
         legend: {
             show: true
         }
     }
 
-    return(
+    return (
         <div>
-             <Chart 
-            options={{...options, labels:chartData.labels}}
-            series={chartData.series}
-            type="donut"
-            height="240"
-        />
+            <Chart
+                options={{ ...options, labels: chartData.labels }}
+                series={chartData.series}
+                type="donut"
+                height="240"
+            />
         </div>
     );
 }
 
-export default donutChart;
+export default DonutChart;
